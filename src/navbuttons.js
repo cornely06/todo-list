162db4fc6f6ee913b56c7d1bafaa84a2db8c766e
Raymond Cornely <cornely06@gmail.com>
2021-08-11 00:53:48 -0400

    Add event listeners to buttons

--- src/navbuttons.js
+++ src/navbuttons.js
@@ -1,17 +1,30 @@
-function createButton(text, id) {
+function createButton(text, id, event) {
     const button = document.createElement('button');
     button.classList.add('navButton');
     button.innerText = text;
     button.value = id;
+    button.addEventListener('click', event);
     return button;
+}
+
+function newProject() {
+    console.log('testing');
+}
+
+function newTask() {
+    console.log('testing');
+}
+
+function projectList() {
+    console.log('testing');
 }
 
 function navButtons() {
     const navButtons = document.createElement('div');
     navButtons.classList.add('navButtons');
-    navButtons.appendChild(createButton('New Project', 'new-project'));
-    navButtons.appendChild(createButton('New Task', 'new-task'));
-    navButtons.appendChild(createButton('Project List', 'project-list'));
+    navButtons.appendChild(createButton('New Project', 'new-project', newProject));
+    navButtons.appendChild(createButton('New Task', 'new-task', newTask));
+    navButtons.appendChild(createButton('Project List', 'project-list', projectList));
     return navButtons;
 }
 
